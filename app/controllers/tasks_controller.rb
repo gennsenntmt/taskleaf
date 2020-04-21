3d3c56803a4fce0fcc48f72a21fbe2c4c5c7ecb1
gennsenntmt <56209451+gennsenntmt@users.noreply.github.com>
2020-04-21 09:39:15 +0900

    task編集機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,14 +14,22 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
   end
 
   private
     def task_params
-      params.require(:task).permit(:name, :desctiption)
+      params.require(:task).permit(:name, :description)
     end
 
 end
